7373ad43050ba057a512ae5d1361078c623f8ec1
Lendesz <lendesz@gmail.com>
2020-11-10 12:17:11 +0100

    maj ex1 avec innerHTML

---
+++ ex1/assets/js/P6-ex1-2.js
@@ -0,0 +1,12 @@
+let eDiv = document.getElementById('emptyDiv');
+
+for (let year = 2020; year <= 2030; year++) {
+
+    let lastDiv = eDiv.innerHTML;
+
+    if ((year%4==0) && ((year%100!=0) || (year%400==0))) {
+        eDiv.innerHTML = '<div style="color:red;">' +lastDiv + year + '</div>';
+    } else { 
+        eDiv.innerHTML = '<div style="color:black;">' +lastDiv + year + '</div>';
+    }
+}
